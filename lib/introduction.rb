d230447ce39a9ffef246bdcf50c2bf35d63c73b6
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-11 17:42:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,10 @@ def introduction(name)
   puts "Hello, my name is #{name}"
 end
 
+introduction("Katie")
+
 def introduction_with_language(name, program)
   puts "Hello, my name is #{name} and I am a #{program} programmer!"
 end
+
+introduction_with_language("Katie", "Ruby")
